178b0eb0275ce576e13feb3d04a9a4d693930d3b
Reuben <118543425+Phyyyyyyyyyyyy@users.noreply.github.com>
2026-03-05 20:08:19 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         
-        Boot gameBooter = new Boot();
+        Boot gameBooter = new Boot();        /* Abstraction imnida */
         gameBooter.launch();
     }
 }
